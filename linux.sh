beafd1e1d46feb9ec6bf3e0bb7bfc0865b6a8f7b
zhang_zi@worksap.co.jp <zhang_zi@worksap.co.jp>
2017-09-27 12:03:50 +0800

    zn

---
+++ linux.sh
@@ -0,0 +1,36 @@
+#builtin
+echo -en
+type -aPpft
+printf "%s %s" $1 $2 
+[while IFS=:] read -rasn:put $1 $2 [; do ; done] <$file
+eval \$$1
+set -- [$vars]
+declare -riafx
+shopt -su
+exec #substitute shell
+caller n
+jobs fg bg
+kill disown wait
+while getopts ":abcde:fg" Option
+do
+  case $Option in
+    a) echo $OPTIND" is next pos";;
+    e) echo $OPTARG;;
+  esac
+done
+
+#external
+ls -StvXRiblF
+cat -vtenbs #tac rev
+cp -ur
+ln -s
+find . -name '' -type f -mtime -atime -ctime [+|-] 1 -maxdepth -inum -perm -exec `` {} [\;|+]
+grep -rnliwZ [-e] $pattern [$file | $dir]
+[grep -lZ | find -print0] | xargs -0n:P:I:tl:p
+expr length match substr index :
+date -ud: +%kMjs
+
+#text
+sort -mnrft -k(f.cp,f.cp) $file1 $file2
+sort | uniq -c | sort -nr
+
