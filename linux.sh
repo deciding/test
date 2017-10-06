71733c2d0a169421de453c5db364096b49756e52
zhang_zi@worksap.co.jp <zhang_zi@worksap.co.jp>
2017-10-06 20:11:20 +0800

    company

--- linux.sh
+++ linux.sh
@@ -21,11 +21,11 @@ done
 
 #external
 ls -StvXRiblF
-cat -vtenbs #tac rev
+cat -vtenbs #tac rev paste join
 cp -ur
 ln -s
 find . -name '' -type f -mtime -atime -ctime [+|-] 1 -maxdepth -inum -perm -exec `` {} [\;|+]
-grep -rnliwZ [-e] $pattern [$file | $dir]
+grep -rnliwZvcHqABoEF [-e] $pattern [$file | $dir]
 [grep -lZ | find -print0] | xargs -0n:P:I:tl:p
 expr length match substr index :
 date -ud: +%kMjs
@@ -33,4 +33,9 @@ date -ud: +%kMjs
 #text
 sort -mnrft -k(f.cp,f.cp) $file1 $file2
 sort | uniq -c | sort -nr
+cut -d:f:,:c:-:
+head -cn
+tail -cnf
 
+sed -en [// | n],[// | n]/[p | d | s/[]/[]/g | y/[]/[]/g ] $file
+sed -en num i str $file
